58d89c62c0f3be6720a24d3680eaaf54009fb4d7
Jesus Blanco Fernandez <jblancofd86@gmail.com>
2018-07-19 23:06:33 +0200

    20% of js

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,24 +10,34 @@ activePlayer = 0;
 
 document.querySelector('.diceSvg').style.display = 'none';
 
+
+document.querySelector('.counter-box-'+ activePlayer).classList.add('active');
+document.querySelector('.icon-turn-'+ activePlayer).classList.add('activeI');
+
+
 document.querySelector('.btn-roll').addEventListener('click', function(){
     var dice = Math.floor(Math.random() * 6) + 1
-    console.log(dice);
-    
+
     document.querySelector('.diceSvg').style.display = 'block';
     document.querySelector('.diceSvg').src = '/resources/css/img/dice_' + dice + '.svg';
 
     if(dice == 1){
-        activePlayer === 0 ? activePlayer === 1 : activePlayer === 0
-        roundScore = 0
+        
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
         console.log(activePlayer);
+        roundScore = 0
         document.querySelector('#current-'+ activePlayer).textContent = roundScore;
-
-         
+        
+        document.querySelector('.counter-box-0').classList.toggle('active');
+        document.querySelector('.counter-box-1').classList.toggle('active');
+        document.querySelector('.icon-turn-0').classList.toggle('activeI');
+        document.querySelector('.icon-turn-1').classList.toggle('activeI');
+        
     } else{
         roundScore += dice
-        console.log(roundScore);
         document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+
+        
 
     }
 

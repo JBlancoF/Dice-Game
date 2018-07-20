8b1baed19fbd490424d8898a7f906ff2ade5b047
Jesus Blanco Fernandez <jblancofd86@gmail.com>
2018-07-20 03:07:45 +0200

    finish js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,46 +1,106 @@
 
-var scores, roundScores, activePlayer,dice;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
 
+
+var scores, roundScore, activePlayer, gameActive ;
+
+gameActive = false
 // document.querySelector('#current-'+ activePlayer).textContent = dice;
 // document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '<em>';
 
-document.querySelector('.diceSvg').style.display = 'none';
 
-
-document.querySelector('.counter-box-'+ activePlayer).classList.add('active');
-document.querySelector('.icon-turn-'+ activePlayer).classList.add('activeI');
-
-
-document.querySelector('.btn-roll').addEventListener('click', function(){
-    var dice = Math.floor(Math.random() * 6) + 1
-
-    document.querySelector('.diceSvg').style.display = 'block';
-    document.querySelector('.diceSvg').src = '/resources/css/img/dice_' + dice + '.svg';
-
-    if(dice == 1){
-        
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        console.log(activePlayer);
-        roundScore = 0
-        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
-        
-        document.querySelector('.counter-box-0').classList.toggle('active');
-        document.querySelector('.counter-box-1').classList.toggle('active');
-        document.querySelector('.icon-turn-0').classList.toggle('activeI');
-        document.querySelector('.icon-turn-1').classList.toggle('activeI');
-        
+//WHICH PLAYER BEGINNINGS
+function playerStart(){
+    var x = Math.random()
+    if(x >= 0.5){
+        return 1
     } else{
-        roundScore += dice
-        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
-
-        
-
+        return 0
     }
+}
+ 
 
-    
+//INITIALITATION OF THE GAME
+function init(){
+    scores = [0,0];
+    gameActive = true;
+    roundScore = 0;
+    activePlayer = playerStart();
+    document.querySelector('.diceSvg').style.display = 'none';
+    document.querySelector('.counter-box-0').classList.remove('active');
+    document.querySelector('.counter-box-1').classList.remove('active');
+    document.querySelector('.icon-turn-0').classList.remove('activeI');
+    document.querySelector('.icon-turn-1').classList.remove('activeI');
 
-})
+    document.querySelector('.counter-box-'+ activePlayer).classList.add('active');
+    document.querySelector('.icon-turn-'+ activePlayer).classList.add('activeI');
+
+    document.getElementById('score-0').textContent = '00';
+    document.getElementById('score-1').textContent = '00';
+    document.getElementById('current-0').textContent = '00';
+    document.getElementById('current-1').textContent = '00';
+    document.querySelector('.player-name-0').textContent = 'Player 1';
+    document.querySelector('.player-name-1').textContent = 'Player 2';
+
+}
+
+//EACH PLAYE HOLDS THEIR POINTS
+function hold(){
+    if(gameActive){
+        scores[activePlayer] += roundScore
+        document.querySelector('#score-'+ activePlayer).textContent = scores[activePlayer];
+        //IF WINS
+        if(scores[activePlayer] >= 10){
+            document.querySelector('.player-name-'+activePlayer).textContent = "WINNER!!"
+            document.querySelector('.diceSvg').style.display = 'none';
+            gameActive = false
+        }else{
+        next() 
+        }
+ 
+    }
+}
+
+//NEXT PLAYER TURNS
+function next(){
+    roundScore = 0
+    document.querySelector('#current-'+ activePlayer).textContent = '00';
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+   
+    document.querySelector('.counter-box-0').classList.toggle('active');
+    document.querySelector('.counter-box-1').classList.toggle('active');
+    document.querySelector('.icon-turn-0').classList.toggle('activeI');
+    document.querySelector('.icon-turn-1').classList.toggle('activeI');
+
+    document.querySelector('.diceSvg').style.display = 'none';
+}
+
+//EACH PLAYE PLAY THE GAME
+function play(){
+    if(gameActive){
+        var dice = Math.floor(Math.random() * 6) + 1
+        document.querySelector('.diceSvg').style.display = 'block';
+        document.querySelector('.diceSvg').src = '/resources/css/img/dice_' + dice + '.svg';
+
+        if(dice == 1){
+            next()
+        } else{
+            roundScore += dice
+            document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+        }
+   
+    }
+}
+
+
+document.querySelector('.btn-roll-0').addEventListener('click', play,);
+document.querySelector('.btn-roll-1').addEventListener('click', play,);
+
+
+document.querySelector('.btn-hold-0').addEventListener('click',hold);
+document.querySelector('.btn-hold-1').addEventListener('click',hold);
+
+document.querySelector('.btn-start').addEventListener('click', init);
+
+
+
